7928098d74216f3a0b43a698d21e16636994fbb6
PES2UG24CS437_4H <samyuktha6002@gmail.com>
2026-04-16 11:16:27 +0530

    Phase 4: serialized commit struct into commit object and wrote it into objects directory using object_write()

--- commit.c
+++ commit.c
@@ -210,6 +210,21 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     snprintf(commit.author, sizeof(c.author), "%s", pes_author());
     commit.timestamp = (uint64_t)time(NULL);
 
+    //Write commit message into the commit struct
+    snprintf(commit.message, sizeof(commit.message), "%s", message);
+
+    //Serialize the commit struct and then create a commit object via object_write
+    void *data;
+    size_t len;
+    if (commit_serialize(&commit, &data, &len) != 0) return -1;
+
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+
+    free(data);
+
     // TODO: Implement commit creation
     // (See Lab Appendix for logical steps)
     (void)message; (void)commit_id_out;
